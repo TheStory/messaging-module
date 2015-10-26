8fa17b5347652b9b25da1fec2df29bde68e835f9
Yaroslav Shatkevich <jarek@thestory.pl>
2015-10-26 13:39:11 +0100

    Modularizing

--- Module.php
+++ Module.php
@@ -1,46 +1,9 @@
 <?php
-
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * Copyright: STORY DESIGN Sp. z o.o.
+ * Author: Yaroslav Shatkevich
+ * Date: 26.10.2015
+ * Time: 13:37
  */
 
-namespace Messaging;
-
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-
-class Module implements AutoloaderProviderInterface
-{
-
-	public function getAutoloaderConfig()
-	{
-		return array(
-			'Zend\Loader\StandardAutoloader' => array(
-				'namespaces' => array(
-					// if we're in a namespace deeper than one level we need to fix the \ in the path
-					__NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/', __NAMESPACE__),
-				),
-			),
-		);
-	}
-
-	public function getConfig()
-	{
-		return include __DIR__ . '/config/module.config.php';
-	}
-
-	public function onBootstrap(MvcEvent $e)
-	{
-		// You may not need to do this if you're doing it elsewhere in your
-		// application
-		$eventManager = $e->getApplication()->getEventManager();
-		$moduleRouteListener = new ModuleRouteListener();
-		$moduleRouteListener->attach($eventManager);
-	}
-
-}
+require_once __DIR__ . '/src/Messaging/Module.php';
